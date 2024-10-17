1da1da209087db814c5559437efa4be497a0508b
SantiBargas <santidariobargas@hotmail.com>
2024-10-16 21:55:12 -0300

    grad_desc

--- Guia6/grad_desc_f1.py
+++ Guia6/grad_desc_f1.py
@@ -7,18 +7,6 @@ def f(x):
 def f_deri(x):
     return -np.sin(np.sqrt(abs(x))) - (x**2 * np.cos(np.sqrt(abs(x))))/(2*abs(x)*np.sqrt(abs(x)))
 
-x = np.linspace(-512,512,100)
-y_f = f(x)
-y_deri_f = f_deri(x)
-
-
-fig,(ax1,ax2) = plt.subplots(2,1,figsize=(10,6))
-ax1.plot(x,y_f)
-ax1.set_title('Función')
-ax2.plot(x,y_deri_f)
-ax2.set_title('Gradiente de la Función')
-plt.show()
-
 # Parámetros del gradiente descendiente
 learning_rate = 0.1   # Constante de aprendizaje
 x = np.random.randint(-512, 512)  # Inicializamos en un número aleatorio del dominio
